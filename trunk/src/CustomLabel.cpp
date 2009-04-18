b79a571cf4340ea64759afa8a3643fba8749f753
vishwajeet.dusane <vishwajeet.dusane@104a2136-2aa3-11de-a435-ffa8d773f76a>
2009-04-18 16:22:16 +0000

    Added sources in repository

---
+++ trunk/src/CustomLabel.cpp
@@ -0,0 +1,55 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Gokul   *
+ *   gokul@linux-7q2y   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "CustomLabel.h"
+
+#include <QIcon>
+#include <QStringList>
+
+#include <QHBoxLayout>
+#include <QWheelEvent>
+
+CustomLabel::CustomLabel(QWidget *parent)
+ : QWidget(parent)
+{
+	
+    QHBoxLayout *horizontalLayout = new QHBoxLayout(this);
+    
+    label = new CImageHolder(this);
+    
+	 horizontalLayout->setMargin(0);
+     horizontalLayout->addWidget(label);
+}
+
+
+CustomLabel::~CustomLabel()
+{
+
+}
+
+void CustomLabel::setPixmap(const QPixmap& image)
+{
+	label->setPixmap(image);
+}
+
+void CustomLabel::setImagePath(const QString& path)
+{
+	label->setImagePath(path);
+}
+

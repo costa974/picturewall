438e87f4aee005edf2da0b684ccb9c6d7309748f
vishwajeet.dusane <vishwajeet.dusane@104a2136-2aa3-11de-a435-ffa8d773f76a>
2009-04-26 19:52:36 +0000

    Added classes to search images on google using google API

---
+++ src/GoogleSearcher.cpp
@@ -0,0 +1,131 @@
+//
+// C++ Implementation: GoogleSearcher
+//
+// Description: 
+//
+//
+// Author: vishwajeet <vishwajeet.dusane@gmail.com>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "GoogleSearcher.h"
+
+#include <QWebFrame>
+
+CGoogleSearcher::CGoogleSearcher(QObject *parent)
+ : QObject(parent)
+{
+	m_pGoogleAJAXapiMedia = new QWebView();
+
+	searchResultLabel = 0;
+	connect(m_pGoogleAJAXapiMedia,SIGNAL(loadFinished(bool)),this,SLOT(searchComplete()));	
+
+	connect(this,SIGNAL(doParsing(QString)),this,SLOT(parse(QString)));	
+
+}
+
+
+CGoogleSearcher::~CGoogleSearcher()
+{
+	delete m_pGoogleAJAXapiMedia;
+}
+
+void CGoogleSearcher::setSearchString(const QString &searchString)
+{
+	this->searchString = searchString;
+}
+
+
+void CGoogleSearcher::search(QString searchString)
+{
+	if(searchString.isEmpty())
+	{
+		searchString = this->searchString;		
+	}else
+	{
+		this->searchString = searchString;
+		searchResultLabel=0;
+		
+	}
+
+
+	if(searchResultLabel > 20)
+	{
+		return;
+	}
+
+
+	QString searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+searchString+"&start="+QString().setNum(searchResultLabel + 4)+"&filter=0&rsz=large";
+	searchUrl = searchUrl.replace(' ','+');
+	m_pGoogleAJAXapiMedia->setUrl(searchUrl);
+	searchResultLabel ++;
+}
+
+void CGoogleSearcher::searchComplete()
+{
+
+ QWebFrame *mainFrame = m_pGoogleAJAXapiMedia->page()->mainFrame();
+ QString frameText = mainFrame->toHtml();
+	
+ emit doParsing(frameText);	
+  search();	
+	
+}
+	
+void CGoogleSearcher::parse(QString rawData)
+{
+    QHash<QString,QString> parsedImageRecord;
+ 
+    QStringList rawChunk = rawData.split("{");	
+
+   			
+	for(int i=0;i<rawChunk.count();++i)
+	{
+		
+		if(!isImageData(rawChunk.at(i)))
+		{
+			
+			continue;
+		}
+
+		parsedImageRecord["url"] = fetchDataFromRawChunk("url",rawChunk.at(i));
+		parsedImageRecord["tbUrl"] = fetchDataFromRawChunk("tbUrl",rawChunk.at(i));
+		parsedImageRecord["tbUrl"].replace("\\u003d","=");
+		emit parsedResult(parsedImageRecord);
+		parsedImageRecord.clear();
+	}
+    
+}
+
+bool CGoogleSearcher::isImageData(const QString &rawData)
+{
+	if(rawData.contains("GsearchResultClass") && rawData.contains("GimageSearch") )
+	{
+		return true;
+	}else
+	{
+		return false;
+	}
+}
+
+QString CGoogleSearcher::fetchDataFromRawChunk(const QString &key,const QString &rawData)
+{
+	QString value;
+	QStringList imageDataList = rawData.split(',');
+
+	for(int i =0;i<imageDataList.count();++i)
+	{
+		if(imageDataList.at(i).contains(QString("\"")+key+QString("\"")))
+		{
+			
+			value = imageDataList.at(i).split("\":\"").at(1);
+			value = value.remove("\"",Qt::CaseInsensitive).remove("}",Qt::CaseInsensitive);
+			return value;
+		}	
+	}	
+
+	return value;
+}
+
+
---
+++ src/HttpImageDownloader.cpp
@@ -0,0 +1,109 @@
+//
+// C++ Implementation: HttpImageDownloader
+//
+// Description: 
+//
+//
+// Author: vishwajeet <vishwajeet.dusane@gmail.com>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "HttpImageDownloader.h"
+
+#include <QUrl>
+
+CHttpImageDownloader::CHttpImageDownloader(const QString &imageUrl,const QString &imageMainUrl,QObject *parent)
+ : QThread(parent)
+{
+    m_ImageUrl = imageUrl;
+    m_ImageMainUrl = imageMainUrl; 	
+    http = new QHttp(this);
+
+    connect(http, SIGNAL(requestFinished(int, bool)),
+            this, SLOT(httpRequestFinished(int, bool)));
+    connect(http, SIGNAL(dataReadProgress(int, int)),
+            this, SLOT(updateDataReadProgress(int, int)));
+    connect(http, SIGNAL(responseHeaderReceived(const QHttpResponseHeader &)),
+            this, SLOT(readResponseHeader(const QHttpResponseHeader &)));
+}
+
+
+CHttpImageDownloader::~CHttpImageDownloader()
+{
+}
+
+void CHttpImageDownloader::run()
+{
+	downloadImage(m_ImageUrl);
+}
+
+
+void CHttpImageDownloader::setDownloadImageUrl(const QString &imageUrl)
+{
+	this->m_ImageUrl = imageUrl;
+
+}
+
+void CHttpImageDownloader::downloadImage(const QString &imageUrl)
+{
+
+   buffer.open(QBuffer::ReadWrite);
+
+	QUrl url(imageUrl);	
+    //QUrl url("http://images.google.com/images?q=tbn:gJEs0EK6FPH7CM:http://topnews.in/light/files/paris_hilton22.jpg");
+   //	QUrl url("http://topnews.in/light/files/paris_hilton22.jpg");
+
+
+    QHttp::ConnectionMode mode = url.scheme().toLower() == "https" ? QHttp::ConnectionModeHttps : QHttp::ConnectionModeHttp;
+    http->setHost(url.host(), mode, url.port() == -1 ? 0 : url.port());
+    
+    if (!url.userName().isEmpty())
+        http->setUser(url.userName(), url.password());
+
+    httpGetId = http->get(url.toString(), &buffer);
+}
+
+
+void CHttpImageDownloader::httpRequestFinished(int requestId, bool error)
+{
+    if (requestId != httpGetId)
+        return;
+
+	emit downloadComplete(buffer.data(),m_ImageMainUrl);
+	
+	buffer.close();	
+ // download complete
+}
+
+void CHttpImageDownloader::readResponseHeader(const QHttpResponseHeader &responseHeader)
+{
+    switch (responseHeader.statusCode()) {
+    case 200:                   // Ok
+    case 301:                   // Moved Permanently
+    case 302:                   // Found
+    case 303:                   // See Other
+    case 307:                   // Temporary Redirect
+        // these are not error conditions
+        break;
+
+        http->abort();
+    }
+}
+
+void CHttpImageDownloader::updateDataReadProgress(int bytesRead, int totalBytes)
+{
+ //   progressDialog->setMaximum(totalBytes);
+ //   progressDialog->setValue(bytesRead);
+	int downloadPercentComplete =0;
+
+	if(bytesRead >1)
+	{
+		downloadPercentComplete = ((double)bytesRead/totalBytes) * 100; 
+	}
+	
+	emit downloading(downloadPercentComplete);
+}
+
+
+
